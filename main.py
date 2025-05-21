e82f2c4b9dc3fbf300321914cd9d10effacf7935
Seth-Parks <bparksghub@gmail.com>
2025-05-20 19:33:39 -0500

    Added books as arguments and refactored code for efficiency

--- main.py
+++ main.py
@@ -1,37 +1,47 @@
 from stats import get_num_words
 from stats import get_char_count
 from stats import sort_char_dict
+import sys
 
+
+#Function to read file
 def get_book_text(file) :
     file_string = file.read()
     return file_string
 
 
-
-
-file_path = 'books/frankenstein.txt'
-
 def main():
-    
+    #Checking system arguments
+    if(len(sys.argv) != 2):
+            print("Usage: python3 main.py <path_to_book>")
+            sys.exit(1)
+
+    file_path = sys.argv[1]
+
+    text_string = None
+
     with open(file_path) as file :
-        #Processimg
+        #Opening file and reading in text as a string
         text_string = get_book_text(file)
-        num_words = get_num_words(text_string)
-        num_chars = get_char_count(text_string)
-        sorted_list = sort_char_dict(num_chars)
-        
-        #Output
-        print("============ BOOKBOT ============")
-        print(f"Analyzing book found at {file_path}")
-        print("----------- Word Count ----------")
-        print(f"Found {num_words} total words")
-        print("--------- Character Count -------")
 
-        for item in sorted_list:
-            if(item['char'].isalpha()):
-                print(f"{item['char']}: {item['num']}")
 
-        print("============= END ===============")
+    #Processing 
+    num_words = get_num_words(text_string)
+    num_chars = get_char_count(text_string)
+    sorted_list = sort_char_dict(num_chars)
+    
+    #Output
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    #Only prints the character if the character is alphabetical
+    for item in sorted_list:
+        if(item['char'].isalpha()):
+            print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 
         
